419952421b03d65449bdd3f6c5d27f21ed1ac4bf
beellz <belal20795@gmail.com>
2020-06-01 22:18:35 +0530

    adding gh-pages and deploy code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+
+
 
 function App() {
+const appid = "0cb40fd4" ;
+const appkey = "36bb35ecba78b9d0473a7473a79d61a9 ";
+const url ="https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}"
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>hello</h1>
+   
     </div>
   );
 }
