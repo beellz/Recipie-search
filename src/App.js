81d12fc25905e3b9d63874c23d30309ccc9f06db
beellz <belal20795@gmail.com>
2020-06-02 21:32:08 +0530

    added to display element with recipejs

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React from 'react';
-
-
-
+import React, { useEffect , useState} from "react" ;
+import Recipie from "./recipie";
 
 
 function App() {
 const appid ="4dd8354a"
 const appkey = "c44a825f9296d024c2e3d8b9088f45d3"
-  // const appid = "0cb40fd4" ;
-
-// const appkey = "36bb35ecba78b9d0473a7473a79d61a9";
-// const url =`https://api.edamam.com/search?q=chicken&app_id=${appid}&app_key=${appkey}`
+const[recipies , setRecipies] = useState([]);
+const url =`https://api.edamam.com/search?q=chicken&app_id=${appid}&app_key=${appkey}`
 
 const getRecipie = async () => {
-
-  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${appid}&app_key=${appkey}`);
+  const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
-
+  console.log(data.hits);
+  setRecipies(data.hits)
 }
-
-getRecipie();
+useEffect(() => {getRecipie();} , []);
 
   return (
     <div className="App">
-      <form className="seacrh-Form">
+      <form className="seacrh-form">
         <input className="Search-bar" type="text" />
         <button type="submit">Search</button>
       </form>
-      
-   
+      {recipies.map(recipe => (
+        <Recipie 
+        key={recipe.recipe.label}
+        title={recipe.recipe.label}
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image} />
+      ))}
+
     </div>
   );
 }
